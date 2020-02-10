9064c146f5a82d37edee6228e693b355c236339f
Atticus <atticusbhat@gmail.com>
2020-02-10 14:03:10 -0800

    Pi calculator benchmark
    yes

---
+++ benchmark.py
@@ -0,0 +1,46 @@
+import time
+import platform
+
+print('Simple Python Benchmark for measureing the CPU speed.')
+
+if "Linux" == platform.system():
+  print('Processor:')
+  with open('/proc/cpuinfo') as f:
+    for line in f:
+        if line.strip():
+            if line.rstrip('\n').startswith('model name'):
+                model_name = line.rstrip('\n').split(':')[1]
+                print(model_name)
+                break
+else:
+  print('Your CPU is only shown automatic on Linux system.')
+
+tries = input( '\nHow many tries per iteration? [def=1000] ')
+if tries == '': tries = 1000
+tries = int(tries)
+iterations = input( 'How many iterations? [def=10] ')
+if iterations == '': iterations = 10
+iterations = int(iterations)
+
+schnitt = 0
+
+for a in range(0,iterations):
+
+  start = time.time()
+
+  for i in range(0,tries):
+    for x in range(1,1000):
+      3.141592 * 2**x
+    for x in range(1,10000):
+      float(x) / 3.141592
+    for x in range(1,10000):
+      float(3.141592) / x
+
+  ende = time.time()
+  dauer = (ende - start)
+  dauer = round(dauer, 3)
+  schnitt += dauer
+  print('Time: ' + str(dauer) + 's')
+
+schnitt = round(schnitt / iterations, 3)
+print('Average: ' + str(schnitt) + 's')
