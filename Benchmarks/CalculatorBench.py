d6304f9aba5d67afc9d7f3b4bc8c8c0aacbe81c7
Atticus <atticusbhat@gmail.com>
2020-02-19 13:52:53 -0800

    j

--- Benchmarks/CalculatorBench.py
+++ Benchmarks/CalculatorBench.py
@@ -1,11 +1,7 @@
 ##modified code from https://github.com/Noxmiles/Python-CPU-Benchmark
-
 import time
-
 import platform
-
 print('Simple Python Benchmark for measureing the CPU speed.')
-
 if "Linux" == platform.system():
   print('Processor:')
   with open('/proc/cpuinfo') as f:
@@ -17,14 +13,12 @@ if "Linux" == platform.system():
                 break
 else:
   print('Your CPU is only shown automatic on Linux system.')
-
 tries = input( '\nHow many tries per iteration? [def=1000] ')
 if tries == '': tries = 1000
 tries = int(tries)
 iterations = input( 'How many iterations? [def=10] ')
 if iterations == '': iterations = 5
 iterations = int(iterations)
-
 bleh = 0
 for a in range(0,iterations):
 	start = time.time()
@@ -35,12 +29,10 @@ for i in range(0,tries):
     float(x) / 3.141592
   for x in range(1,1000):
     float(3.141592) / x
-    
 end = time.time()
 duration = (end - start)
 duration = round(duration, 3)
 bleh += duration
 print('Time: ' + str(duration) + 's')
-
 bleh = round(bleh / iterations, 3)
 print('Average: ' + str(bleh) + 's')
