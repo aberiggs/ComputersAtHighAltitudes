81e4151f9fd9e56ee1d4dfe5e0b5c3808ffc0530
Atticus <atticusbhat@gmail.com>
2020-02-21 12:34:03 -0800

    Added text file output

--- Benchmarks/CalculatorBench.py
+++ Benchmarks/CalculatorBench.py
@@ -10,25 +10,26 @@ tries = int(tries)
 iterations = input( 'How many iterations? [def=10] ')
 if iterations == '': iterations = 5
 iterations = int(iterations)
+with open("data.txt","w+") as f:
+	bleh = 0
+	for a in range(0,iterations):
+		start = time.time()
 
-bleh = 0
-for a in range(0,iterations):
-	start = time.time()
-
-	for i in range(0,tries):
+		for i in range(0,tries):
 		
-		for x in range(1,1000):
-			float(3.141592) * 2**x
-		for x in range(1,1000):
-			float(x) / 3.141592
-		for x in range(1,1000):
-			float(3.141592) / x
+			for x in range(1,1000):
+				float(3.141592) * 2**x
+			for x in range(1,1000):
+				float(x) / 3.141592
+			for x in range(1,1000):
+				float(3.141592) / x
 
-	end = time.time()
-	duration = (end - start)
-	duration = round(duration, 3)
-	bleh += duration
-	print('Time: ' + str(duration) + 's')
+		end = time.time()
+		duration = (end - start)
+		duration = round(duration, 3)
+		bleh += duration
+		print('Time: ' + str(duration) + 's')
+		f.write(str(duration))
 
 bleh = round(bleh / iterations, 3)
 print('total: ' + str(bleh) + 's')
