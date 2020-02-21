b6d383c9796f3b82f5d34b163834d543ee24a438
aberiggs <34726295+aberiggs@users.noreply.github.com>
2020-02-20 19:43:29 -0800

    Create TempTest.sh
    I really hope this works 👀

---
+++ Loggers/TempTest.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+## FEW VARIABLES ########################
+
+NOW=$(date +"%d-%m-%Y-%T")
+
+LOGFILE="/var/log/sensor_temp.log"
+
+
+## THRESHOLDFOR NOTIFICATIONS ##
+## UPON THE SENSORS COMMAND OUTPUT ##
+## WARNING=HIGH 
+## CRITICAL=CRIT
+
+HIGH="60"
+CRIT="100"
+
+## IF HELO SET TO 0 ##
+EHLO="1"
+
+#######################################
+
+## CHECK IF SENSOR IS PRESENT
+
+        hash sensors 2>/dev/null || { echo >&2 "It requires sensor but it's not installed.  Aborting now."; exit 1; }
+
+## READ CPU TEMP ##
+
+        CORE_0=$(sensors | grep 'Core 0' | awk '{print int($3)}')
+        CORE_1=$(sensors | grep 'Core 1' | awk '{print int($3)}')
+
+## ECHO VALUE
+
+        echo "TIME: $NOW"
+        echo "###########################" >> ${LOGFILE}
+        echo "TIME: $NOW" >> ${LOGFILE}
+        echo "CPU Core 0: ${CORE_0} C"
+        echo "CPU Core 0: ${CORE_0} C" >> ${LOGFILE}
+        echo "CPU Core 1: ${CORE_1} C"
+        echo "CPU Core 1: ${CORE_1} C" >> ${LOGFILE}
+
+
+
+## CHECK THE RETURN VALUE FROM EACH CORE; IF NEEDED SEND NOTIFICATION
+
+        if [ "${CORE_0}" -gt ${HIGH} ] || [ ${CORE_1} -gt ${HIGH} ]; 
+                then
+        ## SET VARIABLE FOR EMAIL
+                SUBJECT="***WARNING*** FOR HIGH TEMP FROM ${HOSTNAME} NUC"
+                DATA="Dear User,\r\n\r\nThe temperature for CPU is HIGH: ${CORE_0}C & ${CORE_1}C\r\nPlease Monitor this Machine closer.\r\n\r\nNB Team"
+                email
+                echo "THE TEMPERATURE IS HIGH" >> ${LOGFILE}
+                echo "NOTIFICATION SENT" >> ${LOGFILE}
+                echo "THE TEMPERATURE IS HIGH" 
+        fi
