6523f7bbf79b06ca9a2a6d71af759b75f696d3c8
Sprocket Riggs <aberiggsiv@gmail.com>
2020-02-24 13:53:40 -0800

    Edited Temp Logger

--- Loggers/TempTest.sh
+++ Loggers/TempTest.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 
-
-## FEW VARIABLES ########################
-
-NOW=$(date +"%d-%m-%Y-%T")
-
-LOGFILE="~/Desktop/ComputersAtHighAltitudes/Loggers/sensor_temp.log"
-
-
-## THRESHOLDFOR NOTIFICATIONS ##
-## UPON THE SENSORS COMMAND OUTPUT ##
-## WARNING=HIGH 
-## CRITICAL=CRIT
-
-HIGH="60"
-CRIT="100"
+LOGFILE="home/rpi/Desktop/Data/"
 
 ## IF HELO SET TO 0 ##
 EHLO="1"
 
-#######################################
-
 ## CHECK IF SENSOR IS PRESENT
-
         hash sensors 2>/dev/null || { echo >&2 "It requires sensor but it's not installed.  Aborting now."; exit 1; }
 
-for i in {20}
+for i in {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 19 19 20}
 do
-        ## READ CPU TEMP ##
-
+        NOW=$(date +"%d-%m-%Y-%T")
+		## READ CPU TEMP ##
                 CPU_0=$(vcgencmd measure_temp)
 
-        ## ECHO VALUE
-
+        ## ECHO VALUES 
                 echo "TIME: $NOW"
                 echo "###########################" >> ${LOGFILE}
                 echo "TIME: $NOW" >> ${LOGFILE}
-                echo "CPU: ${CPU_0} C"
-                echo "CPU: ${CPU_0} C" >> ${LOGFILE}
-                
+                echo "CPU: ${CPU_0}"
+                echo "CPU: ${CPU_0}" >> ${LOGFILE}
+				
                 sleep 5
 done
